f5e6d02a1958c7872306bbc4380293c92ff878db
anompermadi <anom.permadi@outlook.com>
2022-02-13 19:09:22 +0700

    update scrape the latest data

--- last_earthquake/__init__.py
+++ last_earthquake/__init__.py
@@ -1,3 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def data_extraction():
     """
     Date: 11 Februari 2022
@@ -9,19 +13,64 @@ def data_extraction():
     Impact: tidak berpotensi TSUNAMI
     :return:
     """
-    res = dict()
-    res["date"] = "11 Februari 2022"
-    res["time"] = "21:25:16 WIB"
-    res["magnitude"] = 5.2
-    res["depth"] = "10 km"
-    res["location"] = {"lat": "5.77 LU",
-                       "lon": "124.43 BT"}
-    res["description"] = "267 km BaratLaut TAHUNA-KEP.SANGIHE-SULUT"
-    res["impact"] = "tidak berpotensi TSUNAMI"
-    return res
+    try:
+        content = requests.get('https://www.bmkg.go.id/')
+    except Exception:
+        return None
+
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+        date_time = soup.find('span', {'class':'waktu'})
+        date_time = date_time.text.split(', ')
+        date = date_time[0]
+        time = date_time[1]
+
+        data = soup.find('div', {'class':'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        data = data.findChildren('li')
+        i = 0
+        magnitude = None
+        depth = None
+        location = None
+        description = None
+        impact = None
+
+        for dat in data:
+            if i == 1:
+                magnitude = dat.text
+            elif i == 2:
+                depth = dat.text
+            elif i == 3:
+                location = dat.text.split(' - ')
+                lat = location[0]
+                long = location[1]
+            elif i == 4:
+                description = dat.text
+            elif i == 5:
+                impact = dat.text
+            i += 1
+
+
+
+        #print(soup.prettify())
+
+        res = dict()
+        res["date"] = date
+        res["time"] = time
+        res["magnitude"] = magnitude
+        res["depth"] = depth
+        res["location"] = {"lat": lat,
+                           "lon": long}
+        res["description"] = description
+        res["impact"] = impact
+        return res
+    else:
+        return None
 
 
 def show_data(result):
+    if result is None:
+        print("Unable to find latest data")
+        return
     print("Last Indonesia earth quake")
     print(f"Date {result['date']}")
     print(f"Time {result['time']}")
